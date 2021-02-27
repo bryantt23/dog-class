5c6088da695b77a28f0da83e32c6a554ebc71d57
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-26 21:54:33 -0800

    add methods

--- test.js
+++ test.js
@@ -1,14 +1,38 @@
 class Dog {
-  constructor(name, breed, age, bark, favorite) {
+  constructor(name, breed, age, bark, favoriteFoods) {
     this.name = name;
     this.breed = breed;
     this.age = age;
     this.bark = bark;
-    this.favorite = favorite;
+    this.favoriteFoods = favoriteFoods;
   }
+
+  set currentAge(age) {
+    this.age = age;
+  }
+
+  isFavoriteFood(food) {
+    console.log(food, this.favoriteFoods);
+    return this.favoriteFoods
+      .map(food => food.toLowerCase())
+      .includes(food.toLowerCase());
+  }
+
+  barks = () => {
+    return this.age > 3 ? this.bark.toUpperCase() : this.bark;
+  };
 }
 
-console.log('hii');
-
 const dog = new Dog('Fido', 'German Shepard', 3, 'Bork!', ['Bacon', 'Chicken']);
-console.log(dog);
+console.log(JSON.stringify(dog));
+console.log(dog.name);
+console.log(dog.barks());
+console.log(dog.favoriteFoods);
+console.log(dog.favoriteFoods);
+dog.currentAge = 42;
+console.log(JSON.stringify(dog));
+console.log(dog.barks());
+console.log(dog.isFavoriteFood('Bacon'));
+console.log(dog.isFavoriteFood('Banana'));
+console.log(dog.isFavoriteFood('bacon'));
+console.log(dog.isFavoriteFood('ChIckEn'));
